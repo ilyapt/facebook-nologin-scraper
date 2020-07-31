37bc0f03c068f54dfa318e9c70b8a8ce1cad0b41
Daniel <gustaw.daniel@gmail.com>
2020-07-31 08:19:34 +0200

    Created integration test

---
+++ tests/integration.test.ts
@@ -0,0 +1,20 @@
+const exec = require('child_process').exec;
+
+it('Zuck Profile should be collected from command line', async () => {
+    const runCommand = `node bin/facebook-nologin-scraper.js https://www.facebook.com/zuck`;
+    const myShellScript = exec(runCommand);
+
+    await new Promise((resolve, reject) => {
+        myShellScript.stdout.on('data', (out) => {
+            // console.log(out);
+            const data = JSON.parse(out);
+            expect(data.name).toBe('Mark Zuckerberg');
+            resolve(data);
+        });
+        myShellScript.stderr.on('data', (error) => {
+            console.error(error);
+            expect(error).toBe(null);
+            reject(error)
+        });
+    });
+})
