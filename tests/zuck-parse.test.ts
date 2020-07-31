fd8ac13e90ec6729b660b58c8740b9d301eecc18
Daniel <gustaw.daniel@gmail.com>
2020-07-31 14:35:36 +0200

    Added Test of Zuck Data

---
+++ tests/zuck-parse.test.ts
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const scraper = require('../lib/facebook-nologin-scraper');
+
+it('Zuck Profile should correctly parsed', async () => {
+    const page = fs.readFileSync(process.cwd() + '/tests/pages/fb-log-zuck.html').toString();
+    const zuckDataString = fs.readFileSync(process.cwd() + '/tests/pages/fb-log-zuck.json').toString();
+
+    const data = scraper(page);
+
+    expect(data.name).toBe('Mark Zuckerberg');
+    expect(data.link).toBe('https://www.facebook.com/zuck');
+    expect(Array.isArray(data.hometown)).toBeTruthy();
+
+    expect(JSON.stringify(data)).toBe(zuckDataString);
+});
